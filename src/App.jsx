3f47b6272b1832102941a9c246b26054e20c4607
Dina Mulyani Putri <dina.mulyani.put@gmail.com>
2022-08-29 00:21:21 -0700

    removed unnecessary

--- src/App.jsx
+++ src/App.jsx
@@ -12,31 +12,30 @@ import Experience from './forms/Experience';
 import OrganisationalExperience from './forms/OrganisationalExperience';
 import OthersExperience from './forms/OthersExperience';
 import PersonalInfoForm from './forms/PersonalInfoForm';
-import ExperienceContextProvider from './contexts/ExperienceContext';
 import MergeContextProvider from './contexts/MergeContext';
-// import StepperContextProvider from './contexts/StepperContext';
+import StepperContextProvider from './contexts/StepperContext';
 
 function App() {
 
   return (
     <Router>
       <div className="App max-w-full bf-violet-100 grid md:grid-cols-2 gap-4">
-        {/* <StepperContextProvider> */}
-        <div>
-          <StepBar />
-          <MergeContextProvider>
-            <Routes>
-              <Route path='/' element={<PersonalInfoForm />} />
-              <Route path='/experience' element={<Experience />} />
-              <Route path='/education' element={<Education />} />
-              <Route path='/organisational' element={<OrganisationalExperience />} />
-              <Route path='/others' element={<OthersExperience />} />
-              <Route path='/complete' element={<Complete />} />
-            </Routes>
-          </MergeContextProvider>
-        </div>
+        <StepperContextProvider>
+          <div>
+            <StepBar />
+            <MergeContextProvider>
+              <Routes>
+                <Route path='/' element={<PersonalInfoForm />} />
+                <Route path='/experience' element={<Experience />} />
+                <Route path='/education' element={<Education />} />
+                <Route path='/organisational' element={<OrganisationalExperience />} />
+                <Route path='/others' element={<OthersExperience />} />
+                <Route path='/complete' element={<Complete />} />
+              </Routes>
+            </MergeContextProvider>
+          </div>
+        </StepperContextProvider>
         <ResumeView />
-        {/* </StepperContextProvider> */}
       </div>
     </Router>
   )
--- src/component/StepBar.jsx
+++ src/component/StepBar.jsx
@@ -10,7 +10,6 @@ const StepBar = () => {
     // });
     // use indexof that support in ie(internet explorer)
     const indexOfPathname = routes.map(route => route.route).indexOf(location.pathname);
-    console.log(indexOfPathname);
 
     return (
         <div className="flex justify-between mb-3 bg-white shadow-md rounded-lg">
@@ -27,26 +26,6 @@ const StepBar = () => {
                     </Link>
                 )
             })}
-            {/* <NavLink to='/experience' className="relative flex flex-col items-center flex-1 group hover:bg-stone-100 cursor-pointer stepper-item">
-                <div className="relative mt-5 z-10 flex justify-center items-center w-12 h-12 rounded-full bg-stone-400 text-white mb-2 circle">2</div>
-                <div className="group-hover:text-stone-600 group-hover:font-normal mb-5 text-xs">Experience</div>
-            </NavLink>
-            <NavLink to='/education' className="relative flex flex-col items-center flex-1 group hover:bg-stone-100 cursor-pointer stepper-item">
-                <div className="relative mt-5 z-10 flex justify-center items-center w-12 h-12 rounded-full bg-stone-400 text-white mb-2 circle">3</div>
-                <div className="group-hover:text-stone-600 group-hover:font-normal mb-5 text-xs">Education</div>
-            </NavLink>
-            <NavLink to='/organisational' className="relative flex flex-col items-center flex-1 group hover:bg-stone-100 cursor-pointer stepper-item">
-                <div className="relative mt-5 z-10 flex justify-center items-center w-12 h-12 rounded-full bg-stone-400 text-white mb-2 circle">4</div>
-                <div className="group-hover:text-stone-600 group-hover:font-normal mb-5 text-xs">Organisational</div>
-            </NavLink>
-            <NavLink to='/others' className="relative flex flex-col items-center flex-1 group hover:bg-stone-100 cursor-pointer stepper-item">
-                <div className="relative mt-5 z-10 flex justify-center items-center w-12 h-12 rounded-full bg-stone-400 text-white mb-2 circle">5</div>
-                <div className="group-hover:text-stone-600 group-hover:font-normal mb-5 text-xs">Others</div>
-            </NavLink>
-            <NavLink to='/complete' className="relative flex flex-col items-center flex-1 group hover:bg-stone-100 cursor-pointer stepper-item">
-                <div className="relative mt-5 z-10 flex justify-center items-center w-12 h-12 rounded-full bg-stone-400 text-white mb-2 circle">6</div>
-                <div className="group-hover:text-stone-600 group-hover:font-normal mb-5 text-xs">Complete</div>
-            </NavLink> */}
         </div>
     );
 }
